946b9b3faf92fdb2dd2d04d0f6184db015773307
Elijah Silva <runsilva@hotmail.com>
2025-12-05 14:56:05 -0500

    Cleaning streamlit script

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,10 +1,17 @@
+# Beverage Web Application
+
+# Purpose: This application serves as a CRUD data entry app that writes to csv files that tracks my daily drinking sessions
+
+# Load packages
 import streamlit as st
 import pandas as pd
 import numpy as np
 import uuid
 
+# Page config
 st.set_page_config(page_title='Beverage Web App', page_icon=':tea:', layout='centered', initial_sidebar_state='auto')
 
+# Page navigation
 activity = ['Log Brew', 'Product Table', 'Add Product', 'Add Vendor', 'Add Order']
 choice = st.sidebar.selectbox('Menu', activity)
 
